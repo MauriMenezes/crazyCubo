bfc6cda6b92e8006509db1955a8dc9cc3600c4c1
Maurilio Souza Menezes <mauriliosm25@gmail.com>
2024-09-25 03:48:27 -0300

    feat: footer component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../../_components/nav-bar/nav-bar.component";
 import { CardsComponent } from '../../_components/cards/cards.component';
+import { FooterComponent } from '../../_components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, CardsComponent],
+  imports: [NavBarComponent, CardsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
